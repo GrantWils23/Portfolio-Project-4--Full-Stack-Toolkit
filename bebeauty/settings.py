8043d5d1505a4c687222ba3fc14888558a3f115f
Grant <GrantWils.23@googlemail.com>
2022-04-17 20:40:59 +0000

    correct spelling mistake in settings.py line 65

--- bebeauty/settings.py
+++ bebeauty/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'bebeauty.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
